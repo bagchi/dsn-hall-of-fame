ba596ddfe1349382b8946d60e36fc584ee3183d1
Your Name <edgardo.barsallo@gmail.com>
2020-06-29 01:39:50 -0400

    Fix format

--- load.js
+++ load.js
@@ -12,14 +12,37 @@ function loadJSON(callback) {
 
 loadJSON(function(response) {
     var data = JSON.parse(response);
+    var table = document.getElementById('rank');
     for(var i=0; i<data.length; i++) {
-        var list = document.getElementById(data[i].anchor);
-        var _item = document.createElement('a');
-        _item.href = data[i].num;
-        _item.innerText = data[i].rank ;
-        var entry = document.createElement('li');
-        entry.appendChild(_item);
-        entry.appendChild(document.createTextNode(", " + data[i].author + ", " + data[i].total + "."));
-        list.appendChild(entry);
+        // var list = document.getElementById(data[i].anchor);
+        // var _item = document.createElement('a');
+        // _item.href = data[i].num;
+        // _item.innerText = data[i].rank ;
+        var row = table.insertRow(i+1)
+        var c0 = row.insertCell(0);
+        var c1 = row.insertCell(1);
+        var c2 = row.insertCell(2);
+        var c3 = row.insertCell(3);
+        var c4 = row.insertCell(4);
+        row.insertCell(5); // filler
+        var c5 = row.insertCell(6);
+
+        c0.innerText = data[i].num
+        c1.innerText = data[i].rank
+        c2.innerText = data[i].author
+        c3.innerText = data[i].total
+        c4.innerText = data[i].recent
+        c5.innerText = data[i].affiliation
+
+        // Style
+        c0.setAttribute("class", "numero");
+        c1.setAttribute("class", "numero");
+        c3.setAttribute("class", "numero");
+        c4.setAttribute("class", "numero");
+
+        // var entry = document.createElement('li');
+        // entry.appendChild(_item);
+        // entry.appendChild(document.createTextNode(", " + data[i].author + ", " + data[i].total + "."));
+        // list.appendChild(entry);
     }
 });
